d654ed51c5a046eb57caf73f0996e4a2cb5c2c5c
carlosadrian00205-bit <carlosadrian00205@gmail.com>
2026-04-17 09:06:02 -0600

    Update app.py
    Primer bloque de verdadero código, este  permite subir el CSV y verlo en pantalla

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Proyecto de estadística")
-st.write("Inicio de mi app.")
+st.title("📊 Asistente de Estadística - UPChiapas")
+
+st.header("1. Carga de datos")
+archivo = st.file_uploader("Sube tu archivo CSV", type=["csv"])
+
+if archivo is not None:
+    df = pd.read_csv(archivo)
+    st.success("¡Archivo cargado!")
+    
+    st.subheader("Vista previa")
+    st.dataframe(df.head())
+    
+    st.header("2. Visualización de Distribuciones")
+    
+    columna = st.selectbox("Selecciona una columna para analizar", df.columns)
+    
+    st.subheader("Análisis de la distribución")
+    pregunta_1 = st.radio("¿La distribución parece normal?", ["Sí", "No", "No estoy seguro"])
+    detalles = st.text_area("¿Hay sesgo u outliers? (Explica lo que ves)")
+    
+else:
+    st.info("Esperando archivo CSV...")
